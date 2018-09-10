ecff8be073516d67c0b5e26cd092a1cd5caf8909
Aadeshkale <aadeshkale0@gmail.com>
2018-09-10 22:41:33 +0530

    load method t load data from js file

---
+++ Sample Database in js from/demo.js
@@ -0,0 +1,37 @@
+db.author.insert({
+	name:'sachin mane',
+	age:22,
+	sal:20000,
+	address:{
+		city:'Aurangabad',
+		state:'Maharastra'
+	}
+});
+
+db.author.insert({
+	name:'shivam madane',
+	age:20,
+	sal:25000,
+	address:{
+		city:'Aurangabad',
+		state:'Maharastra'
+	}
+}),
+db.author.insert({
+	name:'shraddha golwal',
+	age:22,
+	sal:25000,
+	address:{
+		city:'Aurangabad',
+		state:'Maharastra'
+	}
+}),
+db.author.insert({
+	name:'pranita joshi',
+	age:22,
+	sal:20000,
+	address:{
+		city:'Aurangabad',
+		state:'Maharastra'
+	}
+})
